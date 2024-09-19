86a45de5580c6b93e3460a28a4b0774a47ab11f4
Nikhil Patil <patil.nikhil12@outlook.com>
2024-09-19 07:56:27 +0000

    removed comments

--- qbit-monitor.sh
+++ qbit-monitor.sh
@@ -6,11 +6,11 @@ STACK_NAME="pirate"
 PORTAINER_API_URL="https://portainer.nikpatil.com/api"
 PORTAINER_API_KEY="ptr_1AE9TKMLYpcN+MIvrbhiFUHBquoTy9euJnBa8n414rE="
 ENDPOINT_ID="2"
-# Function to get the stack ID
+
 get_stack_id() {
     curl -s -H "X-API-Key:$PORTAINER_API_KEY" "$PORTAINER_API_URL/stacks" | jq -r ".[] | select(.Name==\"$STACK_NAME\").Id"
 }
-# Function to remove the stack
+
 stop_stack() {
     local stack_id=$1
     curl -s -X POST -H "X-API-Key: $PORTAINER_API_KEY" "$PORTAINER_API_URL/stacks/$stack_id/stop?endpointId=$ENDPOINT_ID"
@@ -19,7 +19,7 @@ start_stack() {
     local stack_id=$1
     curl -s -X POST -H "X-API-Key: $PORTAINER_API_KEY" "$PORTAINER_API_URL/stacks/$stack_id/start?endpointId=$ENDPOINT_ID"
 }
-# Check if the port is open
+
 (echo > /dev/tcp/$ADDRESS/$PORT) &>/dev/null
 if [ $? -ne 0 ]; then
     echo "Port check failed, restarting stack..."
